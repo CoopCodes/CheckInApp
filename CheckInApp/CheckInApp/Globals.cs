83318bbe0e94a7ef629d5caeb510094d995313cc
CoopCodes <gc021217@goldcoastcc.qld.edu.au>
2022-07-31 14:40:26 +1000

    Import Files

---
+++ CheckInApp/CheckInApp/Globals.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections;
+
+namespace CheckInApp {
+    static class Globals {
+        public static Dictionary<string, Models.EventType> EventTypes { get; set; }
+
+        public static Models.Student Student { get; set; }
+        public static int DeviceID { get; set; }
+        public static ArrayList Students { get; set; }
+        public static ArrayList Devices { get; set; }
+        public static ArrayList DeviceIDs { get; set; }
+
+        static Globals() {
+            try {
+                Student = new Models.Student(0, "", "", "", 0);
+                Students = (ArrayList)Query.Read("spStudents_Read", Query.FindAll, null);
+                Devices = (ArrayList)Query.Read("spDevices_Read", Query.FindAll, null);
+                EventTypes = (Dictionary<string, Models.EventType>)Query.Read("spEventTypes_Read", Query.FindAll, null);
+            }
+            catch {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
---
+++ CheckInApp/CheckInApp/Models/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CheckInApp.Models {
+    public class Student {
+        public int StudentID { get; set; }
+
+        public string Username { get; set; }
+
+        public string Firstname { get; set; }
+
+        public string Lastname { get; set; }
+
+        public int Grade { get; set; }
+
+        public Student(int studentID, string username, string firstname, string lastname, int grade) {
+            Username = username;
+            Firstname = firstname;
+            Lastname = lastname;
+            StudentID = studentID;
+            Grade = grade;
+        }
+    }
+}
