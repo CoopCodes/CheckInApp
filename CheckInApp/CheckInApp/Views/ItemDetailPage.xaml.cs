0d4018dc7181bf6e5f79a43cdcc42c201203258c
CoopCodes <gc021217@goldcoastcc.qld.edu.au>
2022-07-31 13:01:17 +1000

    ol

---
+++ CheckInApp/CheckInApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using CheckInApp.ViewModels;
+
+namespace CheckInApp.Views {
+    public partial class ItemDetailPage : ContentPage {
+        public ItemDetailPage() {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ CheckInApp/CheckInApp/Views/NewItemPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using CheckInApp.Models;
+using CheckInApp.ViewModels;
+
+namespace CheckInApp.Views {
+    public partial class NewItemPage : ContentPage {
+        public Item Item { get; set; }
+
+        public NewItemPage() {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
